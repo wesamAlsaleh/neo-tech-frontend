2279afb940e2b0c80b7fbd68f72fc52e742b5272
wesamAlsaleh <wesammuneer7@gmail.com>
2025-01-28 15:58:54 +0300

    Implement Manage Products page with product fetching and display functionality

---
+++ src/services/products-services.ts
@@ -0,0 +1,26 @@
+// import axios to make http requests
+import axios from "axios";
+
+// import response type
+import { Products } from "@/types/product";
+
+/**
+ * @function getProducts to get all products
+ */
+export const getProducts = async () => {
+  try {
+    // make a get request to the server
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_APP_URI}/products`
+    );
+
+    if (response.status !== 200) {
+      return "Error fetching products from the server side";
+    }
+    // return the data from the response which is an array of products
+    return response.data;
+  } catch (error) {
+    // if there is an error, log the error
+    console.error(error);
+  }
+};

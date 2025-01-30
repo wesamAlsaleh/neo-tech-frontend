09c4f4d78c65d9f2e6205d26a4a9a4644288f2cd
wesamAlsaleh <wesammuneer7@gmail.com>
2025-01-31 01:36:34 +0300

    Add new button icons for add, edit, remove, and shopping basket actions

---
+++ public/icons.ts
@@ -0,0 +1,19 @@
+import addBasket50 from "../public/assets/button-icons/icons8-add-basket-50.png";
+import addBasket100 from "../public/assets/button-icons/icons8-add-basket-100.png";
+import removeBasket50 from "../public/assets/button-icons/icons8-shopping-basket-remove-50.png";
+import removeBasket100 from "../public/assets/button-icons/icons8-shopping-basket-remove-100.png";
+import edit50 from "../public/assets/button-icons/icons8-edit-50.png";
+import edit100 from "../public/assets/button-icons/icons8-edit-100.png";
+import delete50 from "../public/assets/button-icons/icons8-remove-50.png";
+import delete100 from "../public/assets/button-icons/icons8-remove-100.png";
+
+export const icons = {
+  addBasket50,
+  addBasket100,
+  removeBasket50,
+  removeBasket100,
+  edit50,
+  edit100,
+  delete50,
+  delete100,
+};

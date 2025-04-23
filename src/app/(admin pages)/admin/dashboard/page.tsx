0537a04df45fefd25027f00bb42ff32c5d888e0c
wesamAlsaleh <wesammuneer7@gmail.com>
2025-04-23 17:46:50 +0300

    Refactor dashboard page to add the initial layout for the widget sections

--- src/app/(admin pages)/admin/dashboard/page.tsx
+++ src/app/(admin pages)/admin/dashboard/page.tsx
@@ -6,17 +6,63 @@ import React from "react";
 // import custom components
 import PageTitle from "@/components/PageTitle";
 import { ActionButton } from "@/components/ActionButton";
+import Card from "@/components/Card";
 
 export default function dashboardPage() {
   return (
-    <div>
-      <PageTitle
-        title="Dashboard"
-        subtitle="Manage your site settings and view analytics"
-        actionButton={<ActionButton href="/home" text="Home" />}
-      />
+    // Dashboard Page Layout
+    <div className="flex flex-col gap-4">
+      {/* Grid Layout */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {/* Left Container */}
+        <div className="flex flex-col gap-4">
+          {/* Total Users Graph Widget Section */}
+          <div>
+            <Card
+              CardTitle="Total Users"
+              CardDescription="↑ 12% from last week"
+              CardHight={"h-[200px] md:h-[300px]"}
+            />
+          </div>
+        </div>
 
-      <h2>Content here soon</h2>
+        {/* Right Container */}
+        <div className="flex flex-col gap-4">
+          {/* Sales Graph Widget Section */}
+          <div>
+            <Card CardTitle="Sales" CardHight={"h-[200px] md:h-[300px]"} />
+          </div>
+        </div>
+      </div>
+
+      {/* Task Manager Widget Section */}
+      <div>
+        <Card
+          CardTitle="Pending Orders"
+          CardHight={
+            "h-[200px] md:h-[300px] lg:h-[400px] xl:h-[500px] 2xl:h-[600px]"
+          }
+        />
+      </div>
+
+      {/* Grid Layout */}
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        {/* Left Container */}
+        <div className="flex flex-col gap-4">
+          {/* System Status Widget Section */}
+          <div>
+            <Card CardTitle="System Status" />
+          </div>
+        </div>
+
+        {/* Right Container */}
+        <div className="flex flex-col gap-4">
+          {/* Revenue Breakdown Widget Section */}
+          <div>
+            <Card CardTitle="Revenue Breakdown" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

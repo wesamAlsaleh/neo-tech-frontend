92c8aff03c9a58319514280bd42df0408c12209c
wesamAlsaleh <wesammuneer7@gmail.com>
2025-04-30 00:48:17 +0300

    Add loading skeleton layout to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,6 +9,7 @@ interface CardProps {
   CardFooter?: string | React.ReactNode;
   CardHeight?: string | number; // Height of the card (e.g., "h-64", "h-auto")
   CardMaxContentHeight?: string | number; // Max height for content area (e.g., "max-h-64")
+  loading?: boolean; // Loading state for the card
 }
 
 export default function Card({
@@ -19,7 +20,35 @@ export default function Card({
   CardFooter,
   CardHeight = "h-auto",
   CardMaxContentHeight = "max-h-64", // Default max height for content area
+  loading = false, // Loading state for the card
 }: CardProps) {
+  // Loading state for the card
+  if (loading) {
+    return (
+      // Loading Card Layout
+      <div
+        className={`rounded-xl border bg-white shadow-sm ${CardHeight} flex flex-col`}
+      >
+        {/* Title & Description Loading Container */}
+        <div className="px-6 pt-4 pb-2 flex-none space-y-2">
+          {/* Title Loading Skeleton */}
+          <div className="h-7 w-1/2 bg-gray-200 rounded animate-pulse" />
+
+          {/* Description Loading Skeleton */}
+          <div className="h-4 w-3/4 bg-gray-200 rounded animate-pulse" />
+        </div>
+
+        {/* Content Loading Container  */}
+        <div
+          className={`px-6 py-4 flex-grow ${CardMaxContentHeight} space-y-3`}
+        >
+          {/* Content skeleton */}
+          <div className="h-full w-full bg-gray-200 rounded animate-pulse" />
+        </div>
+      </div>
+    );
+  }
+
   return (
     // Card Layout with fixed height if specified
     <div

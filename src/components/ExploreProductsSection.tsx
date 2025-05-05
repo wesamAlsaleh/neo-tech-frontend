5dfb0b77c9a89adc029dcf662e452065ee94d72c
wesamAlsaleh <wesammuneer7@gmail.com>
2025-05-06 01:53:53 +0300

    Refactor ExploreProductsSection to manage loading state

--- src/components/ExploreProductsSection.tsx
+++ src/components/ExploreProductsSection.tsx
@@ -18,17 +18,45 @@ export default function ExploreProductsSection() {
   // Router instance
   const router = useRouter();
 
+  // State to manage the loading state
+  const [loading, setLoading] = useState(true);
+
+  // State to manage products
   const [products, setProducts] = useState<Product[]>([]);
 
-  // fetch all categories
-  useEffect(() => {
-    const fetchExploreProducts = async () => {
+  // Function to fetch best selling products from the server
+  const fetchExploreProducts = async () => {
+    try {
+      // Call the server function to get the best selling products
       const serverResponse = await getExploreProducts();
 
-      if (serverResponse.status) setProducts(serverResponse.products);
+      // If the server response is successful, set the products state
+      if (serverResponse.status) {
+        setProducts(serverResponse.products);
+      } else {
+        // If the server response is not successful, set the products state to an empty array
+        setProducts([]);
+
+        // To make the loading skeleton appear in case of an error
+        setLoading(true);
+      }
+    } catch (error) {
+      // To make the loading skeleton appear in case of an error
+      setLoading(true);
+    } finally {
+      // Set loading to false after fetching data
+      setLoading(false);
+    }
+  };
+
+  // Fetch data from server
+  useEffect(() => {
+    const initFetch = async () => {
+      // Fetch the user cart data from the server
+      await fetchExploreProducts();
     };
 
-    fetchExploreProducts();
+    initFetch();
   }, []);
 
   return (
@@ -39,7 +67,7 @@ export default function ExploreProductsSection() {
       />
 
       {/* Product Cards Grid */}
-      <ProductsGridLayout products={products} />
+      <ProductsGridLayout products={products} isLoading={loading} />
 
       {/*  View All Products Button */}
       <div className="flex justify-center mt-4">
